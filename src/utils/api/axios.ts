9fb08f45595a6ea6d9970865fe42e3fb824ed7c4
Softenrj <rjsharmase@gmail.com>
2025-09-06 19:28:51 +0530

    feat: Add Axios for API requests and implement API utility functions

---
+++ src/utils/api/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import { defaultApiRoute } from '../constance'
+
+const defaultAxios = axios.create({baseURL: defaultApiRoute})
+
+defaultAxios.interceptors.request.use(
+    async (config) => {
+        const token = localStorage.get('bricks:auth')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+export default defaultAxios
