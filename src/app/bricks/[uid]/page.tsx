f33302415d15fc9f59c2abb4ce7a568b0182f06c
Softenrj <rjsharmase@gmail.com>
2025-12-10 12:52:56 +0530

    feat: Add crossOrigin attribute to AvatarImage components for CORS compliance

--- src/app/bricks/[uid]/page.tsx
+++ src/app/bricks/[uid]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='flex w-screen h-screen items-center justify-center text-white text-6xl font-extrabold'>Live Soon..</div>
+    <div className='flex bg-black w-screen h-screen items-center justify-center text-white text-6xl font-extrabold'>Live Soon..</div>
   )
 }
 
